d819438cac01600e9182b8eb2627250e5ed29513
zattaoui <zakaria.attaoui@midgard-ai.com>
2022-10-19 23:21:45 +0200

    Utilisation des functions

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
-  <h1 :class="h1Title" :id="unid">Bonjour monde !</h1>
-  <h1 v-bind="obj" >Bonjour monde 2 !</h1>
-  <input :type="inputType"/>
+  <h1> {{ getTitle() }}</h1>
+  <input :type="inputType()"/>
 </template>
 
 <script setup lang="ts">
 
-const h1Title = 'montitre2';
-const unid = 'monid';
-const key = 'class';
-const inputType = 'date';
+const inputType = () => {
+  return 'number'
+};
 
-
-const obj = {
-  'class': h1Title,
-  'id': unid
-}
+const getTitle = () => {
+  return 'Hello World'
+};
 
 </script>
 
