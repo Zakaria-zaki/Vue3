c025da5e4d6caeb138a4dc08db079f1db1886e2e
zattaoui <zakaria.attaoui@midgard-ai.com>
2022-10-19 23:18:27 +0200

    v-bind (obj ..)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <h1> Hello</h1>
-  <div v-html="name"></div>
+  <h1 :class="h1Title" :id="unid">Bonjour monde !</h1>
+  <h1 v-bind="obj" >Bonjour monde 2 !</h1>
+  <input :type="inputType"/>
 </template>
 
 <script setup lang="ts">
-const name = '<strong>Zakaria</strong>';
+
+const h1Title = 'montitre2';
+const unid = 'monid';
+const key = 'class';
+const inputType = 'date';
+
+
+const obj = {
+  'class': h1Title,
+  'id': unid
+}
+
 </script>
 
 <style></style>
