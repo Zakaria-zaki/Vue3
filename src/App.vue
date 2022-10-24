4844293d9c062029dc0559885b35126d9b32441f
zattaoui <zakaria.attaoui@midgard-ai.com>
2022-10-24 23:59:42 +0200

    function reactive ..

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
-    <p>Zakaria test</p>
+    <h1> Hello {{ user.name  }}</h1>
+    <h2> Count: {{ state.count }} </h2>
+    <button @click="add">Add</button>
 </template>
 
 <script setup lang="ts">
+import { nextTick, reactive } from 'vue';
+
+const state  = reactive({
+    user: {
+        name: 'zakaria',
+        age: 25
+    },
+    count: 0
+})
+
+let { user } = state
+
+const add = () => {
+    user.name = user.name === 'zakaria' ? 'Salim' : 'zakaria' 
+    state.count++;
+    nextTick(() => {
+        console.log('tick');
+    })
+}
+
+
 </script>
 
 <style></style>
