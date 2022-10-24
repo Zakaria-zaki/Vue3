d17f2fa03630821ebbe88d2306857835e93158b5
Zakaria-zaki <40574656+Zakaria-zaki@users.noreply.github.com>
2022-10-25 01:28:27 +0200

    Revert "Chapitre 4"

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,21 @@
 <template>
-    <input type="number" v-model="product.quantity" />
-    <h2>Prix HT: {{ prixHT }}</h2>
-    <h2>Prix TTC: {{ prixTTC }}</h2>
+  <form @submit.prevent="submit()">
+  <button>Submit</button>
+  </form>
+
+  <input @keyup.enter="mykeyup()"/>
 </template>
 
 <script setup lang="ts">
-import { computed, reactive, watch, watchEffect } from 'vue';
 
-interface Product {
-    name: string,
-    quantity: number,
-    priceHT: number,
-    nbOfModification: number,
-    dateOfMod?: number 
+const submit = () => {
+  console.log('submit')
 }
 
-const product = reactive<Product>({
-    name: 'Book',
-    quantity: 3,
-    priceHT: 10,
-    nbOfModification: 0,
-});
-
-const prixHT = computed(() => product.priceHT * product.quantity)
-const prixTTC = computed(() => product.priceHT * product.quantity * 1.2 )
-
-const unwatch = watch(() => product.quantity, (new1, old) => {
-    product.nbOfModification++;
-    console.log(new1);
-    console.log(old);
-})
-
-watchEffect(() => {
-    product.dateOfMod = Date.now()
-})
+const mykeyup = () => {
+  console.log('mykeyup')
+}
 
 </script>
 
 <style></style>
- 
