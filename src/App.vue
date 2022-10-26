d5c47ec76ff6b6cfc9a2ac8417ecc3efd8f2f62a
zattaoui <zakaria.attaoui@midgard-ai.com>
2022-10-27 01:02:13 +0200

    Scoped Css ..

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,10 @@
 <template>
-    <input type="number" v-model="product.quantity" />
-    <h2>Prix HT: {{ prixHT }}</h2>
-    <h2>Prix TTC: {{ prixTTC }}</h2>
+    <h1>Bonjour Zakaria </h1>
 </template>
 
 <script setup lang="ts">
-import { computed, reactive, watch, watchEffect } from 'vue';
-
-interface Product {
-    name: string,
-    quantity: number,
-    priceHT: number,
-    nbOfModification: number,
-    dateOfMod?: number 
-}
-
-const product = reactive<Product>({
-    name: 'Book',
-    quantity: 3,
-    priceHT: 10,
-    nbOfModification: 0,
-});
-
-const prixHT = computed(() => product.priceHT * product.quantity)
-const prixTTC = computed(() => product.priceHT * product.quantity * 1.2 )
-
-const unwatch = watch(() => product.quantity, (new1, old) => {
-    product.nbOfModification++;
-    console.log(new1);
-    console.log(old);
-})
-
-watchEffect(() => {
-    product.dateOfMod = Date.now()
-})
-
 </script>
 
-<style></style>
+<style scoped>
+</style>
  
