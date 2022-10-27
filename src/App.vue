a4db03ed13d9de37c6ac83bf4e9efe5b690476bf
zattaoui <zakaria.attaoui@midgard-ai.com>
2022-10-28 01:29:35 +0200

    la liaison de style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
-    <h1>Bonjour Zakaria </h1>
+    <button @click="Add()">+</button>
+    <h1 :style="{
+        fontSize: fontSize + 'px'
+    }">Bonjour Zakaria </h1>
+    <button @click="min()">-</button>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const fontSize = ref(10)
+
+const Add = () => fontSize.value += 10
+
+const min = () => fontSize.value -= 10
 </script>
 
 <style scoped>
