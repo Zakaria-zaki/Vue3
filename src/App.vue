689dc3888d004b33d18a38744ad687c87a093c92
zattaoui <zakaria.attaoui@midgard-ai.com>
2022-11-02 23:53:13 +0100

    Using of sass

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
-    <button @click="Add()">+</button>
-    <h1 :style="{
-        fontSize: fontSize + 'px'
-    }">Bonjour Zakaria </h1>
-    <button @click="min()">-</button>
+    <h1><span>Bonjour</span> Zakaria </h1>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-
-const fontSize = ref(10)
-
-const Add = () => fontSize.value += 10
-
-const min = () => fontSize.value -= 10
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+h1 {
+    color: red;
+    span {
+        color: blue
+    }
+}
 </style>
  
