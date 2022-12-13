1c9e60f83a9a62ce4ecf330e8aee6ade5abe3029
zattaoui <zakaria.attaoui@midgard-ai.com>
2022-12-13 16:45:30 +0100

    send data from parent to child with defineProps

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-    <Product />  
-    <Product />  
+    <Product :product="product"/> 
 </template>
 
 <script setup lang="ts">
 import Product from './Product.vue'
+import { reactive } from 'vue';
+
+const product = reactive({
+    available: true,
+    prix: 1500,
+    name: "Voiture"
+})
+
+
 </script>
 
 <style></style>
--- src/Product.vue
+++ src/Product.vue
@@ -1,6 +1,19 @@
 <template>
-    <h1>zakaria</h1>
+    <h1>{{ title }}</h1>
+    <h2>{{ product.prix }}</h2>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+
+export interface ProductInterface{
+    available: boolean;
+    prix: number;
+    name: string;
+}
+
+const props = defineProps<{ product: ProductInterface }>();
+
+const title = computed(() => props.product.name.toUpperCase());
+
 </script>
